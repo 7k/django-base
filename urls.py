a46d42b6b7789cab2e3f149a428badb7f27fd825
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2017-08-11 09:06:32 +0100

    Stop using patterns() in urls
    Change-Id: I74fe98a32e01ef6c9c1e29338fdf5d6261dbcc7b

--- urls.py
+++ urls.py
@@ -1,15 +1,14 @@
 '''URLconf for base app.
 '''
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from base.views import PageDetail, Home
 
-urlpatterns = patterns(
-    'base.views',
+urlpatterns = [
     url(r'^(?P<slug>[-\w]+)/$',
         view=PageDetail.as_view(),
         name='page'),
     url(r'^$',
         view=Home.as_view(),
         name='home'),
-)
+]
